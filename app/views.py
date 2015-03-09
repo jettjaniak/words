74697f2e413f2311278a7028151a68c60b963e70
Kajetan Janiak <kajetan.janiak@gmail.com>
2015-03-09 20:38:53 +0100

    jQuery mobile replaced with Bootstrap, Translation creating view added

--- app/views.py
+++ app/views.py
@@ -1,7 +1,37 @@
-from django.views.generic import ListView
-from app.models import Translation
+from django import forms
+from django.views.generic import ListView, FormView
+from app.models import Translation, Word, Language
 
 
 class TranslationListView(ListView):
     model = Translation
     template_name = 'translation_list.html'
+
+
+class TranslationCreateForm(forms.Form):
+    # TODO: validator
+    english = forms.CharField()
+    polish = forms.CharField()
+
+
+class TranslationCreateView(FormView):
+    template_name = 'translation_create.html'
+    form_class = TranslationCreateForm
+    success_url = '/translation-create/'
+
+    def form_valid(self, form):
+        polish_words = form.cleaned_data['polish'].split(',')
+        english_words = form.cleaned_data['english'].split(',')
+        polish = Language.objects.get(code='pl')
+        english = Language.objects.get(code='en')
+        words_ids = []
+        for p in polish_words:
+            w = Word.objects.get_or_create(language=polish, word=p)[0]
+            words_ids += [w.id]
+        for e in english_words:
+            w = Word.objects.get_or_create(language=english, word=e)[0]
+            words_ids += [w.id]
+        t = Translation()
+        t.save()
+        t.words.add(*words_ids)
+        return super(TranslationCreateView, self).form_valid(form)
--- words/urls.py
+++ words/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from app.views import TranslationListView
+from app.views import TranslationListView, TranslationCreateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='base.html')),
     url(r'^translations/$', TranslationListView.as_view()),
+    url(r'^translation-create/$', TranslationCreateView.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 )
